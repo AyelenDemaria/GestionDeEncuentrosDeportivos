220cec7809cd654965a1cc034afcf34289f573b8
julisteck <justeckinger@gmail.com>
2022-11-28 22:13:43 -0300

    Eliminacion tabs y puesta a punto proyecto

--- GED.FrontEnd/GED-Ionic/src/app/app-routing.module.ts
+++ GED.FrontEnd/GED-Ionic/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  {path: 'home' , component: AppComponent}
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
