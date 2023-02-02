a4a92bc30bd296bfc838362bfa9b52f7c556eb37
julisteck <justeckinger@gmail.com>
2023-02-01 22:14:17 -0300

    agrego componente home, mis voucher, agrego diseño

---
+++ GED.FrontEnd/GED-Ionic/src/app/components/mis-voucher/mis-voucher.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-voucher',
+  templateUrl: './mis-voucher.component.html',
+  styleUrls: ['./mis-voucher.component.scss'],
+})
+export class MisVoucherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
