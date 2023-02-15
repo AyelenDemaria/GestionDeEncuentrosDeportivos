d0dcc73e659853697559dd576fc4dd93a0694485
Ayelen <ayedemaria@gmail.com>
2023-02-14 21:16:23 -0300

    agrego pyhtonanywhere en settings

--- GED.BackEnd/GED_Django/settings.py
+++ GED.BackEnd/GED_Django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-xh=n$!slh7r87k$f_bo)=l4g)w-a-$$&$8^w6fn%r^&c-1wtan
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
