975d5cadef207425b756c98a2399872dac6abd81
Ayelen <ayedemaria@gmail.com>
2023-03-19 19:35:42 -0300

    agrego id en get usuarios

--- GED.BackEnd/usuarios/serializers.py
+++ GED.BackEnd/usuarios/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password', 'first_name', 'last_name', 'is_staff']
+        fields = ["id",'username', 'password', 'first_name', 'last_name', 'is_staff']
 
 class PerfilSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False)
