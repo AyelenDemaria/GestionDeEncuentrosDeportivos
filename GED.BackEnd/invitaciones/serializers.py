4a0bbcedeafefab07ad87389d902c5f8a5647394
Ayelen <ayedemaria@gmail.com>
2023-03-27 18:53:26 -0300

    agrego descripciones en invitaciones

--- GED.BackEnd/invitaciones/serializers.py
+++ GED.BackEnd/invitaciones/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Invitacion
-from partidos.serializers import PartidoSerializer
+from partidos.serializers import PartidoSerializer, PartidoGetSerializer
 from usuarios.serializers import PerfilSerializer
 
 class InvitacionSerializer(serializers.ModelSerializer):
@@ -9,7 +9,7 @@ class InvitacionSerializer(serializers.ModelSerializer):
         fields = ["usuario_invita","usuario_invitado","partido","fecha_hora_invitacion","estado"]
 
 class InvitacionGetSerializer(serializers.ModelSerializer):
-    partido = PartidoSerializer(many=False)
+    partido = PartidoGetSerializer(many=False)
     usuario_invita = PerfilSerializer(many=False)
     class Meta:
         model = Invitacion
