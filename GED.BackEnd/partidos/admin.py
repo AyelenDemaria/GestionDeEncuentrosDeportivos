398b451819bbecbcf69fafcfd3da52ccaa053410
Ayelen <ayedemaria@gmail.com>
2023-04-15 17:37:05 -0300

    agrego confirmado en partido y descuento puentos desde admin

--- GED.BackEnd/partidos/admin.py
+++ GED.BackEnd/partidos/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import Partido
+from inscripciones.models import Inscripcion
+from usuarios.models import Perfil
+from django.utils import timezone
 
-# Register your models here.
-admin.site.register(Partido)
+class PartidoAdmin(admin.ModelAdmin):
+    exclude = ('fecha_hora_suspendido', 'fecha_hora_confirmado')
 
-"""class MyAdminView(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)"""
+        if obj.confirmado == False and obj.fecha_hora_confirmado is None:
+            print("partido", obj.id)
+            obj.fecha_hora_confirmado = timezone.localtime(timezone.now())
+            inscripciones = Inscripcion.objects.filter(partido_id = obj.id)
+            if inscripciones:
+                for i in inscripciones:
+                    perfil = Perfil.objects.get(id=i.jugador_id)
+                    if i.jugador_id == obj.creador_id:
+                        perfil.puntos_acum -= 10
+                        perfil.save()
+                    else:
+                        perfil.puntos_acum -= 5
+                        perfil.save()
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Partido, PartidoAdmin)
--- GED.BackEnd/partidos/models.py
+++ GED.BackEnd/partidos/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from canchas.models import Cancha
 from usuarios.models import Perfil
 from tipos_partidos.models import Tipo_partido
+from django.contrib import admin
 
 # Create your models here.
 class Partido(models.Model):
@@ -12,9 +13,16 @@ class Partido(models.Model):
     creador = models.ForeignKey(Perfil, on_delete=models.CASCADE)
     cancha = models.ForeignKey(Cancha, on_delete=models.CASCADE)
     suspendido = models.BooleanField(default=False)
+    fecha_hora_suspendido = models.DateTimeField(null=True)
+    confirmado =  models.BooleanField(default=True)
+    fecha_hora_confirmado = models.DateTimeField(null=True)
+
 
     def __str__(self):
         return str(self.fecha_hora) + ' - ' + str(self.cancha) + ' - ' + str(self.tipo_partido)
+
+class PartidoAdmin(admin.ModelAdmin):
+    fields =  ['id', 'fecha_hora', 'cant_jugadores', 'tipo_partido', 'creador', 'cancha', 'suspendido', 'fecha_hora_suspendido','confirmado','fecha_hora_confirmado']
 
 class InscriptosPartido(models.Model):
     partido = models.ForeignKey(Partido, on_delete=models.CASCADE)
